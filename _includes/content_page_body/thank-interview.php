6a68195649d9b0a96f1d469d83c50f7ee56911d6
MIV <front.igor.m@gmail.com>
2018-10-05 17:25:45 +0500

    thank

--- _includes/content_page_body/thank-interview.php
+++ _includes/content_page_body/thank-interview.php
@@ -2,4 +2,42 @@
     .top-bar-container, footer { display: none; }
 </style>
 
-THANKYOU interview
+<div class="full-wh" style="overflow-y: auto; position: fixed; left: 0; top: 0; z-index: 1;">
+
+    <div style="display: table; width: 100%; height: 100%;">
+        <div style="display: table-cell; vertical-align: middle; text-align: center; padding: 30px 20px;">
+
+            <div class="full-width" style="font-size: 30px; color: #414654;">
+
+                Благодарим Вас за заявку на курс
+
+            </div>
+
+            <div class="full-width" style="font-size: 30px; color: #414654;">
+
+                <b>«Брокер торговой недвижимости»</b> в тарифе <b>«ГАРАНТ»</b>
+
+            </div>
+
+            <div class="full-width" style="font-size: 24px; color: #2d9cdb;">
+
+                В ближайшее время мы вышлем на почту анкету для заполнения.
+
+            </div>
+
+            <div class="full-width" style="font-size: 24px; color: #2d9cdb;">
+
+                Ждите звонка от нашего менеджера для согласования даты и времени интервью.
+
+            </div>
+
+            <div class="full-width">
+
+                interview
+                
+            </div>
+
+        </div>
+    </div>
+
+</div>
--- _includes/content_page_body/thank-sum.php
+++ _includes/content_page_body/thank-sum.php
@@ -2,4 +2,36 @@
     .top-bar-container, footer { display: none; }
 </style>
 
-THANKYOU sum
+<div class="full-wh" style="overflow-y: auto; position: fixed; left: 0; top: 0; z-index: 1;">
+
+    <div style="display: table; width: 100%; height: 100%;">
+        <div style="display: table-cell; vertical-align: middle; text-align: center; padding: 30px 20px;">
+
+            <div class="full-width" style="font-size: 30px; color: #414654;">
+
+                Благодарим Вас за заявку на курс
+
+            </div>
+
+            <div class="full-width" style="font-size: 30px; color: #414654;">
+
+                <b>«Брокер торговой недвижимости»</b> в тарифе <b>«БИЗНЕС»</b>
+
+            </div>
+
+            <div class="full-width" style="font-size: 24px; color: #2d9cdb;">
+
+                Ближайшее время менеджер свяжется с вами для выставления счёта.
+
+            </div>
+
+            <div class="full-width">
+
+                sum
+
+            </div>
+
+        </div>
+    </div>
+
+</div>
