e90af5e1a1ed28751ce0564e9a599066df5d3d84
MIV <front.igor.m@gmail.com>
2018-10-05 17:13:09 +0500

    thank

--- _includes/content_page_body/thank-score.php
+++ _includes/content_page_body/thank-score.php
@@ -2,4 +2,47 @@
     .top-bar-container, footer { display: none; }
 </style>
 
-THANKYOU score
+<div class="full-wh" style="overflow-y: auto; position: fixed; left: 0; top: 0; z-index: 1;">
+
+    <div style="display: table; width: 100%; height: 100%;">
+        <div style="display: table-cell; vertical-align: middle; text-align: center; padding: 30px 20px;">
+
+            <div class="full-width" style="font-size: 30px; color: #414654;">
+
+                Благодарим Вас за заявку на курс
+
+            </div>
+
+            <div class="full-width" style="font-size: 30px; color: #414654;">
+
+                <b>«Брокер торговой недвижимости»</b> в тарифе <b>«БИЗНЕС»</b>
+
+            </div>
+
+            <div class="full-width" style="font-size: 24px; color: #2d9cdb;">
+
+                За 72 часа до старта курса мы вышлем ссылку на оплату оставшейся суммы.
+
+            </div>
+
+            <div class="full-width" style="font-size: 24px; color: #2d9cdb;">
+
+                Чтобы получить ссылку раньше, просто напишите об этом на почту sales@rbs-24.ru
+
+            </div>
+
+            <div class="full-width">
+
+                1
+
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
