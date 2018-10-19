7d16b228cc3f708b15e55acf59c47b4e566fc762
MIV <front.igor.m@gmail.com>
2018-10-19 18:23:42 +0500

    ed footer data

--- _includes/content_page_header_footer/footer.php
+++ _includes/content_page_header_footer/footer.php
@@ -7,11 +7,27 @@
 
                 <div style="display: inline-block; vertical-align: top;">
 
-                    <img src="<? echo $path; ?>/images/footer/logo-footer.png" alt="" draggable="false">
+                    <div style="margin-bottom: 40px;">
+                        <img src="<? echo $path; ?>/images/footer/logo-footer.png" alt="" draggable="false">
+                    </div>
+
+                    <a href="http://facebook.com" target="_blank" style="margin-left: 8px; font-family: 'Circe-Light', sans-serif; font-size: 20px; color: #ffffff; text-decoration: none !important;">
+                        <img src="<? echo $path; ?>/images/footer/facebook.png" alt="" draggable="false">
+                    </a>
+                    <a href="http://youtube.com" target="_blank" style="margin-left: 8px; font-family: 'Circe-Light', sans-serif; font-size: 20px; color: #ffffff; text-decoration: none !important;">
+                        <img src="<? echo $path; ?>/images/footer/youtube.png" alt="" draggable="false">
+                    </a>
+                    <a href="http://instagram.com" target="_blank" style="margin-left: 8px; font-family: 'Circe-Light', sans-serif; font-size: 20px; color: #ffffff; text-decoration: none !important;">
+                        <img src="<? echo $path; ?>/images/footer/instagram.png" alt="" draggable="false">
+                    </a>
 
                 </div>
 
-                <div style="display: inline-block; vertical-align: top; margin-left: 120px; padding-top: 0px;">
+                <div style="display: inline-block; vertical-align: top; margin-left: 120px; padding-top: 10px;">
+
+                    <div class="full-width" style="margin-bottom: 20px; font-size: 20px; color: #ffffff;">
+                        <a href="<? echo $path; ?>/legal/1" style="font-family: 'Circe-Light', sans-serif; font-size: 20px; color: #ffffff;">Правовая информация</a>
+                    </div>
 
                     <img src="<? echo $path; ?>/images/footer/mailto-icon.png" alt="" draggable="false"><a href="mailto:sale@rbs-24.ru" style="margin-left: 12px; font-size: 16px; color: #ffffff;">sale@rbs-24.ru</a>
 
@@ -23,22 +39,21 @@
 
             </div>
 
-            <div style="display: inline-block; vertical-align: top; float: right;">
+            <div style="display: inline-block; vertical-align: top; float: right; width: 600px;">
 
                 <div style="display: inline-block; vertical-align: top; margin-right: 80px; padding-top: 14px;">
-                    <a href="<? echo $path; ?>/legal/1" style="font-family: 'Circe-Light', sans-serif; font-size: 20px; color: #ffffff;">Правовая информация</a>
+
+
+
                 </div>
 
-                <div style="display: inline-block; vertical-align: top; padding-top: 10px;">
-                    <a href="http://facebook.com" target="_blank" style="margin-left: 8px; font-family: 'Circe-Light', sans-serif; font-size: 20px; color: #ffffff; text-decoration: none !important;">
-                        <img src="<? echo $path; ?>/images/footer/facebook.png" alt="" draggable="false">
-                    </a>
-                    <a href="http://youtube.com" target="_blank" style="margin-left: 8px; font-family: 'Circe-Light', sans-serif; font-size: 20px; color: #ffffff; text-decoration: none !important;">
-                        <img src="<? echo $path; ?>/images/footer/youtube.png" alt="" draggable="false">
-                    </a>
-                    <a href="http://instagram.com" target="_blank" style="margin-left: 8px; font-family: 'Circe-Light', sans-serif; font-size: 20px; color: #ffffff; text-decoration: none !important;">
-                        <img src="<? echo $path; ?>/images/footer/instagram.png" alt="" draggable="false">
-                    </a>
+                <div style="display: inline-block; vertical-align: top; padding-top: 10px; text-align: right; color: #cccccc;">
+
+                    ООО "Бизнес-Школа недвижимости"<br>
+                    ИНН/КПП 9710066718/771001001<br>
+                    ОГРН 1187746755668<br>
+                    Россия, Московская область, г. Москва, ул. Тверская д.7, офис 401
+
                 </div>
 
             </div>
