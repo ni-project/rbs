87e22001c5d7beccab3d812f1bb7feac0a994480
MIV <front.igor.m@gmail.com>
2018-10-26 22:10:06 +0500

    tt

--- _includes/content_head/title-function.php
+++ _includes/content_head/title-function.php
@@ -10,8 +10,8 @@ function setTitle($name) {
         case 'price':
             $title = 'Прайс';
             if ( isset($_GET['thankyou']) ) { $title = 'Спасибо'; }
-            if ( $_GET['name'] == 'brokerige' ) { $title = 'PRO Бизнес на аренде: ОНЛАНЙ-курс по брокериджу коммерческой недвижимости'; }
-            if ( $_GET['name'] == 'brokerigerate' ) { $title = 'PRO Бизнес на аренде: ОНЛАНЙ-курс по брокериджу коммерческой недвижимости'; }
+            if ( $_GET['name'] == 'brokerige' ) { $title = 'PRO Бизнес на аренде: ОНЛАЙН-курс по брокериджу коммерческой недвижимости'; }
+            if ( $_GET['name'] == 'brokerigerate' ) { $title = 'PRO Бизнес на аренде: ОНЛАЙН-курс по брокериджу коммерческой недвижимости'; }
             break;
         default:
             $title = 'RBS';
